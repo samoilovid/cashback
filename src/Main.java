f5a45791728290b6c4c4db5ddbdcecd3ba72777f
Student <student@itpark.ru>
2019-09-14 15:52:12 +0300

    fixes #1 Logic with limit added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-    int amountInRegular = 100;
-    int amountInHigh = 1000;
-    int amountInSpecial = 10_000;
+        int amountInRegular = 100;
+        int amountInHigh = 1000;
+        int amountInSpecial = 10_000;
 
-    int percentInRregular = 1;
+        int percentInRregular = 1;
         int percentInHigh = 5;
         int percentInSpecial = 30;
-        int result = (amountInRegular*percentInRregular+
-                amountInHigh*percentInHigh +
-                amountInSpecial*percentInSpecial)/100;
-        System.out.println(result);
+        int result = (amountInRegular * percentInRregular +
+                amountInHigh * percentInHigh +
+                amountInSpecial * percentInSpecial) / 100;
+        int cashback;
+        if (result > 3_000) {
+            cashback = 3000;
+        } else {
+            cashback = 3000;
+        }
+            System.out.println(cashback);
+        }
+
+
     }
-}
